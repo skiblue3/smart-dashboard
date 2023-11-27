723cec124b9a0432eb03d3b3bb958963238d1334
skiblue3 <myzticblue126@gmail.com>
2023-11-28 00:13:19 +0530

    done changes

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -44,7 +44,7 @@ function WeatherCard() {
     result.humidity = weatherData.main.humidity;
   }
 
-  console.log('weather', API_KEY, result);
+  console.log('weather', result);
 
   const currentDate = new Date();
   const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
@@ -57,21 +57,8 @@ function WeatherCard() {
         <div className="font-bold text-xl">GCT Coimbatore</div>
         <div className="text-sm text-gray-500">{formattedDate}</div>
         <div className="mt-6 text-6xl self-center inline-flex items-center justify-center rounded-lg text-indigo-400 h-24 w-24">
-          {/* <svg
-            className="w-32 h-32"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M3 15a4 4 0 004 4h9a5 5 0 10-.1-9.999 5.002 5.002 0 10-9.78 2.096A4.001 4.001 0 003 15z"
-            ></path>
-          </svg> */}
-          <img src={result.iconLink} alt="Weather Icon" />
+         
+          <img src={result.iconLink} alt="Weather Icon" className="w-[80%]"/>
         </div>
         <div className="flex flex-row items-center justify-center mt-6">
           <div className="font-medium text-6xl">{result.curr_temp}°</div>
