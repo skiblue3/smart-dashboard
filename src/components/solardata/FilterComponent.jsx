2ef97e455beb18ac420211da32f7f0edf3425cd3
Thirumurugan <muruganraj1705@gmail.com>
2023-12-10 22:16:39 +0530

    update filter component

---
+++ src/components/solardata/FilterComponent.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import { DatePicker } from 'antd';
+import axios from "axios";
+
+function FilterComponent({filterHandler}) {
+    let [fromdate, setFromdate] = useState(null);
+    let [enddate, setEnddate] = useState(null);
+    const DateChangeHandler = (val) => {
+        const inputDateString = val;
+        const inputDate = new Date(inputDateString);
+        const year = inputDate.getFullYear();
+        const month = String(inputDate.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+        const day = String(inputDate.getDate()).padStart(2, '0');
+        const formattedDate = `${year}/${month}/${day}`; // Output: 2023/12/05
+        return formattedDate;
+    }
+    const fetchData = async() =>{
+        const res = await axios.post("https://smart-energy-dashboard-backend.onrender.com/api/solar/filterData",{
+            startdate:fromdate,
+            enddate:enddate
+        });
+        console.log(res.data.data.length);
+
+        filterHandler(res.data);
+    }
+
+    return (
+        <div className="flex mr-3 px-3 py-1 gap-3 items-center">
+            <div className="flex items-center gap-2 ">
+                <DatePicker
+                    style={{
+                        width: '100%',
+                        height: '40px'
+                    }}
+                    onChange={(e) => {
+                        console.log(e.$d);
+                        setFromdate(DateChangeHandler(e.$d));
+                    }}
+                />
+            </div>
+            <div className="text-blue-400">To</div>
+            <div className="flex items-center gap-2 ">
+                <DatePicker
+                    style={{
+                        width: '100%',
+                        height: '40px'
+                    }}
+                    onChange={(e) => {
+                        console.log(e.$d);
+                        setEnddate(DateChangeHandler(e.$d));
+                    }}
+                />
+            </div>
+            <div>
+                <button
+                    type="button"
+                    className=" px-4 py-2 items-center gap-x-2 text-sm font-semibold rounded-md focus:outline-none  outline-none bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                    onClick={()=>{fetchData()}}
+                >
+                    Apply
+                </button>
+            </div>
+
+
+        </div>
+    );
+}
+
+export default FilterComponent;
--- src/pages/SolarData.jsx
+++ src/pages/SolarData.jsx
@@ -3,14 +3,17 @@ import TableComponent from '../components/solardata/TableComponent';
 // import data from "./output.json";
 import axios from 'axios';
 import DownloadButton from '../components/solardata/DownloadButton';
+import FilterComponent from '../components/solardata/FilterComponent';
+import { Modal } from 'antd';
 
 const SolarPanelPage = () => {
   const [data, setData] = useState(null);
+  console.log(process.env.REACT_APP_BACKEND_URL);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(process.env.REACT_APP_BACKEND_URL + 'api/solar/data');
+        const response = await axios.get('https://smart-energy-dashboard-backend.onrender.com/api/solar/data');
         if (response.status === 200) {
           setData(response.data); // Store the fetched data in state
         } else {
@@ -24,8 +27,24 @@ const SolarPanelPage = () => {
     fetchData(); // Fetch data when the component mounts
   }, []);
 
+  const filterHandler = (filtered_data) =>{
+    if (filtered_data.length!==0){
+      setData(filtered_data);
+    }else{
+      Modal.warning({
+        title:"No data",
+        content:"There is no data on the specified date."
+      })
+    }
+  }
+
+
+
   if (!data) {
-    return <div>Loading...</div>; // Or any other loading indicator
+    return <div className='md:flex md:items-center md:justify-between'>
+      {/* <DownloadButton data={parsedData} /> */}
+      <FilterComponent />
+    </div>; // Or any other loading indicator
   }
 
   const parsedData = data.data.map((obj) => {
@@ -35,7 +54,11 @@ const SolarPanelPage = () => {
 
   return (
     <div className="w-[90%] h-[85vh]">
-      <DownloadButton data={parsedData} />
+
+      <div className='md:flex md:items-center md:justify-between'>
+        <DownloadButton data={parsedData} />
+        <FilterComponent filterHandler={filterHandler}  />
+      </div>
       <div className="overflow-auto">
         <TableComponent data={parsedData} />
       </div>
